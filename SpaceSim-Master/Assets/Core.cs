2962256adbef52a2352d3911da90412ee652e99f
Thimble <anthonyeboothroyd@gmail.com>
2023-06-17 00:27:05 +0100

    Create Core.cs

---
+++ SpaceSim-Master/Assets/Core.cs
@@ -0,0 +1,14 @@
+public class Core{
+   public enum CurrentBehaviour {
+        Eating, Drinking, Playing, Defecating, Working, Walking, Idling
+    }
+    public enum Impulses {
+        Hunger, Water, Happiness, Bladder, Oxygen
+    }
+    public enum Solutions {
+        Fridge, Sink, Play, Toilet, LifeSupport, Chair
+    }
+    public enum Responsibility {
+        Captain, Engineer, Gunner, Medic, Mediator 
+    }
+}
