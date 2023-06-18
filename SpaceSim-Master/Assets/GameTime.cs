c85a30240ed166914e5c53416f85b7a75930a658
Thimble <anthonyeboothroyd@gmail.com>
2023-06-18 02:42:44 +0100

    Update GameTime.cs

--- SpaceSim-Master/Assets/GameTime.cs
+++ SpaceSim-Master/Assets/GameTime.cs
@@ -6,32 +6,21 @@ public class GameTime : MonoBehaviour
     public delegate void OnTickHandler();
     public static event OnTickHandler OnTck;
 
+    private static bool Active { get; set; } = false;
     private float Timer {get; set;} = 0.0f;
-    
-    // interval in seconds
     private readonly float interval = 0.6f;
-
-    private static bool active = false;
-
-    public static void ClockStart() => active = true;
-    public static void ClockStop() => active = false;
-    //public static void ClockReset() => timer = 0.0f;
-
-    private void Awake()
-    {
-        ClockStart();
-    }
-
+    
+    private void Awake() => ClockStart();
+    
     public void Update() {
-
-        if (active == false)
+        if (Active == false)
             return;
-
         Timer += Time.deltaTime;
-
         if (Timer >= interval) {
             Timer -= interval;
             OnTck?.Invoke();
         }
     }
+    public static void ClockStart() => Active = true;
+    public static void ClockStop() => Active = false;
 }
