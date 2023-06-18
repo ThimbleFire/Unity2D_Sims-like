c017f7c96a87485acf8b6ff9b8fc087bc1df46d6
Thimble <anthonyeboothroyd@gmail.com>
2023-06-18 02:32:13 +0100

    Update Facility.cs

--- SpaceSim-Master/Assets/Facility.cs
+++ SpaceSim-Master/Assets/Facility.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Facility : MonoBehaviour {
 
-    public enum Type {
+    public enum EType {
+        Undefined,
         Toilet, 
         Fridge, 
         Sink, 
@@ -12,30 +13,28 @@ public class Facility : MonoBehaviour {
         Navigations, 
         LifeSupport, 
         CaptainsChair
-    }
+    };
     
-    public Type m_type;
+    public EType Type {get; set;} = EType.Undefined;
     public bool Engaged { get; set; } = false;
     public bool Broken { get; set; } = false;
     public Vector3Int Coordinates { get; set; } = Vector3Int.zero;
     
     /// <summary> When an NPC starts an interaction, roll to see whether facility breaks </summary>
-    public void InteractStart() {
-        Engaged = true;
-    }
-    public void InteractEnd() {
-        Engaged = false;
-    }
+    public virtual void InteractStart() => GameTime.OnTck += GameTime_OnTick;
+    public virtual void InteractEnd() => GameTime.OnTck -= GameTime_OnTick;
+    protected virtual void GameTime_OnTick() { }
 }
 
 public static class Facilities {
 
     private static List<Facility> FacilityList = new List<Facility>();
     
-    public static void Sort() => FacilityList.Sort();
-    public static void Add(Facility f) => FacilityList.Add(f);
-    public static void Remove(Facility f) => FacilityList.Remove(f);
-    public static void Remove(Vector3Int coordinates) => Facility.RemoveAll( x => x.Coordinates == coordinates);
-    public static Facility Get(Vector3Int coordinates) =>   return FacilityList.FindAll( x => x.Coordinates == coordinates) != null ? FacilityList.FindAll( x => x.Coordinates == coordinates)[0] : null;
-    public static Facility Get(Facility.Type t) =>          return FacilityList.FindAll( x => x.m_type == t) != null ? FacilityList.FindAll( x=> x.m_type == t) : null;
+    public static void Sort()                                        =>        FacilityList.Sort();
+    public static void Add(Facility f)                               =>        FacilityList.Add(f);
+    public static void Add(GameObject prefab, Vector3 worldPosition) =>        FacilityList.Add( GameObject.Instantiate(prefab, worldPosition, Quaternion.Identify).GetComponent<Facility>() );
+    public static void Remove(Facility f)                            =>        FacilityList.Remove(f);
+    public static void Remove(Vector3Int coordinates)                =>        FacilityList.RemoveAll( x => x.Coordinates == coordinates);
+    public static Facility Get(Vector3Int coordinates)               => return FacilityList.FindAll( x => x.Coordinates == coordinates) != null ? FacilityList.FindAll( x => x.Coordinates == coordinates)[0] : null;
+    public static Facility Get(Facility.Type t)                      => return FacilityList.FindAll( x => x.m_type == t) != null ? FacilityList.FindAll( x=> x.m_type == t) : null;
 }
