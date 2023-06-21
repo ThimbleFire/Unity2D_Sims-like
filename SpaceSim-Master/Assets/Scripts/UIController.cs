d418ea409e06f6f240518f51b1ae4af872b400b2
Thimble <anthonyeboothroyd@gmail.com>
2023-06-21 02:06:31 +0100

    Update UIController.cs
    Tidied up code. Might contains some bugs, like Vector2Int += Vector2Int

--- SpaceSim-Master/Assets/Scripts/UIController.cs
+++ SpaceSim-Master/Assets/Scripts/UIController.cs
@@ -20,39 +20,31 @@ public class UIController : MonoBehaviour
         NPCs,
         None
     };
-
     public static BuildWindow SelectedBuildWindow { get; set; } = BuildWindow.Floor;
     private static BuildWindow LastSelectedBuildWindow { get; set; } = BuildWindow.Floor;
     private ArrowKeysControlling arrowKeysControlling { get; set; } = ArrowKeysControlling.Cursor; 
     public static Entity SelectedEntity { get; set; }
     public static EnvironmentElement activeElement = null;
     private Facility activeFacility = null;
-
     private byte selectedTabIndex = 0;
     private byte selectedItemIndex = 0;
-
     private bool canPlace = false;
-
     public Sprite selected;
     public Sprite unselected;
-
     public GameObject buildInterface;
     public GameObject NPCInspectorInterface;
-
     public TMPro.TMP_InputField NPCNameField;
     public Image[] NPCRoles;
-
     public Image[] tabs;
     public GameObject[] panels;
-
     public Transform cursor;
     public RectTransform buildMenuCursor;
-    public static int CoordinatesX = 5;
-    public static int CoordinatesY = 5;
-    public static Vector3Int Coordinates { get { return new Vector3Int( CoordinatesX, CoordinatesY, 0 ); } }
-
-    private void Awake() => MakeActiveController();
-
+    public static Vector3Int Coordinates { get; set; }
+    
+    private void Awake() {
+        Coordinates = new Vector3Int(5, 5, 0);
+        MakeActiveController();
+    }
     public void MakeActiveController() {
         Controller.OnWDown += BtnArrowUp;
         Controller.OnDDown += BtnArrowRight;
@@ -61,7 +53,6 @@ public class UIController : MonoBehaviour
         Controller.OnRDown += BtnConfirm;
         Controller.OnFDown += BtnBack;
     }
-
     public void RelinquishController() {
         Controller.OnDDown -= BtnArrowRight;
         Controller.OnWDown -= BtnArrowUp;
@@ -69,92 +60,45 @@ public class UIController : MonoBehaviour
         Controller.OnADown -= BtnArrowLeft;
         Controller.OnRDown -= BtnConfirm;
     }
-
-    private void BtnArrowUp() {
-        switch( arrowKeysControlling ) {
-            case ArrowKeysControlling.Cursor:
-                CoordinatesY++;
-                UpdateCursor();
-                break;
-            case ArrowKeysControlling.CursorBuildMode:
-                CoordinatesY++;
-                UpdateCursor();
-                OnMouseOverCoordinateChange( Coordinates );
-                break;
-        }
-    }
-    private void BtnArrowDown() {
-        switch( arrowKeysControlling ) {
-            case ArrowKeysControlling.Cursor:
-                CoordinatesY--;
-                UpdateCursor();
-                break;
-            case ArrowKeysControlling.CursorBuildMode:
-                CoordinatesY--;
-                UpdateCursor();
-                OnMouseOverCoordinateChange( Coordinates );
-                break;
-        }
-    }
-
+    private void BtnArrowUp() => UpdateCursor(Vector3Int.up);
+    private void BtnArrowDown() => UpdateCursor(Vector3Int.down);
     private void BtnArrowLeft() {
         switch( arrowKeysControlling ) {
             case ArrowKeysControlling.Cursor:
-                CoordinatesX--;
-                UpdateCursor();
+            case ArrowKeysControlling.CursorBuildMode:
+                UpdateCursor(Vector3Int.left);
                 break;
             case ArrowKeysControlling.Tabs:
-                selectedTabIndex--;
-                BtnTab( selectedTabIndex );
+                UpdateTab(-1);
                 break;
             case ArrowKeysControlling.Item:
-                if( selectedItemIndex > 0 ) {
-                    buildMenuCursor.position = panels[selectedTabIndex].transform.GetChild( --selectedItemIndex ).position;
-                    buildMenuCursor.sizeDelta = panels[selectedTabIndex].transform.GetChild( selectedItemIndex ).GetComponent<Image>().sprite.rect.size + Vector2.one * 2;
-                }
+                UpdateItem(-1);
                 break;
             case ArrowKeysControlling.NPCInspector:
-                break;
-            case ArrowKeysControlling.CursorBuildMode:
-                CoordinatesX--;
-                UpdateCursor();
-                OnMouseOverCoordinateChange( Coordinates );
                 break;
         }
     }
     private void BtnArrowRight() {
         switch( arrowKeysControlling ) {
             case ArrowKeysControlling.Cursor:
-                CoordinatesX++;
-                UpdateCursor();
+            case ArrowKeysControlling.CursorBuildMode:
+                UpdateCursor(Vector3Int.right);
                 break;
             case ArrowKeysControlling.Tabs:
-                selectedTabIndex++;
-                BtnTab( selectedTabIndex );
+                UpdateTab(1);
                 break;
             case ArrowKeysControlling.Item:
-                if( selectedItemIndex < panels[selectedTabIndex].transform.childCount - 1 ) {
-                    buildMenuCursor.position = panels[selectedTabIndex].transform.GetChild( ++selectedItemIndex ).position;
-                    buildMenuCursor.sizeDelta = panels[selectedTabIndex].transform.GetChild(selectedItemIndex).GetComponent<Image>().sprite.rect.size + Vector2.one * 2;
-                }
+                UpdateItem(1);
                 break;
             case ArrowKeysControlling.NPCInspector:
                 break;
-            case ArrowKeysControlling.CursorBuildMode:
-                CoordinatesX++;
-                UpdateCursor();
-                OnMouseOverCoordinateChange( Coordinates );
-                break;
         }
-    }
-    
+    }    
     private void BtnConfirm() {
-
         switch( arrowKeysControlling ) {
             case ArrowKeysControlling.Cursor:
                 // it's possible to be in cursor mode while the editor is open, so have this check to ensure we're doing it right
                 Facility f = Facilities.Get( Coordinates );
-
                 if( f != null ) {
                     // this will cause an error if we select an actual facility and not an NPC
                     // though we'll update this later so we can move facilities instead of deleting and replacing
@@ -164,7 +108,6 @@ public class UIController : MonoBehaviour
                     arrowKeysControlling = ArrowKeysControlling.NPCInspector;
                     return;
                 }
-
                 if( !buildInterface.activeSelf ) {
                     arrowKeysControlling = ArrowKeysControlling.Tabs;
                     cursor.gameObject.SetActive( false );
@@ -219,22 +162,32 @@ public class UIController : MonoBehaviour
                 break;
         }
     }
-
-    private void UpdateCursor() {
-        Vector3 offset = new Vector3(0.04f, 0.04f);
+    private void UpdateCursor(Vector3Int addition) {
+        Coordinates += addition;
         Vector3 worldPosition = Helper.CellToWorld(Coordinates);
+        Vector3 offset = new Vector3(0.04f, 0.04f);
         cursor.position = worldPosition + offset;
+        if(arrowKeysControlling == ArrowKeysControlling.CursorBuildMode) {
+            OnMouseOverCoordinateChange( Coordinates );
+        }
     }
-
+    private void UpdateTab(byte addition) {
+        selectedTabIndex += addition;
+        BtnTab(selectedTabIndex);
+    }
+    private void UpdateItem(byte addition) {
+        if( selectedItemIndex > 0 && selectedItemIndex < panels[selectedTabIndex].transform.childCount - 1 ) {
+            buildMenuCursor.position = panels[selectedTabIndex].transform.GetChild( ++selectedItemIndex ).position;
+            buildMenuCursor.sizeDelta = panels[selectedTabIndex].transform.GetChild(selectedItemIndex).GetComponent<Image>().sprite.rect.size + Vector2.one * 2;
+        }
+    }
     public void ShowBuildMenu() {
         GameTime.ClockStop();
         buildInterface.SetActive( true );
         SelectedBuildWindow = LastSelectedBuildWindow;
         Controller.OnWDown -= BtnArrowUp;
         Controller.OnSDown -= BtnArrowDown;
-
     }
-    
     public void HideBuildMenu() {
         canPlace = false;
         buildInterface.SetActive( false );
@@ -248,13 +201,9 @@ public class UIController : MonoBehaviour
         activeFacility = null;
         SelectedBuildWindow = BuildWindow.None;
     }
-
     public void ShowNPCInspector() {
-
         NPCInspectorInterface.SetActive( true );
-
         NPCNameField.text = SelectedEntity.Name;
-
         for( int i = 0; i < 5; i++ )
             NPCRoles[i].color = SelectedEntity.Responsibilities[i] ? Color.yellow : Color.black;
     }
@@ -262,11 +211,8 @@ public class UIController : MonoBehaviour
         NPCInspectorInterface.SetActive( false );
         SelectedEntity = null;
     }
-
     public void BtnTab( byte index ) {
-
         SelectedBuildWindow = ( BuildWindow )index;
-
         for( int i = 0; i < 4; i++ ) {
             if( i == index ) {
                 tabs[i].sprite = selected;
@@ -277,14 +223,10 @@ public class UIController : MonoBehaviour
             }
         }
     }
-
     private void PlaceObjectIntoScene( Vector3Int newCoordinate ) {
-
         if( !canPlace )
             return;
-
         activeFacility.GetComponent<SpriteRenderer>().color = Color.white;
-
         // give a box collider component to NPCs so we can select them
         CrewBehaviour isNPC = activeFacility.gameObject.GetComponent<CrewBehaviour>();
         if( isNPC != null ) {
@@ -296,7 +238,6 @@ public class UIController : MonoBehaviour
                 for( int x = 0; x < activeFacility.Size.x; x++ )
                     Pathfind.Occupy( newCoordinate + new Vector3Int( x, y, 0 ) );
         }
-
         arrowKeysControlling = ArrowKeysControlling.Item;
         buildMenuCursor.gameObject.SetActive( true );
         Controller.OnWDown -= BtnArrowUp;
@@ -307,12 +248,9 @@ public class UIController : MonoBehaviour
         ShowNPCInspector();
     }
     private void OnMouseOverCoordinateChange( Vector3Int newCoordinate ) {
-
         activeFacility.transform.position = Helper.CellToWorld(newCoordinate) + new Vector3( 0.04f, 0.04f );   
         activeFacility.Coordinates = newCoordinate;
-
         canPlace = true;
-
         for( int y = 0; y > -activeFacility.Size.y; y-- )
             for( int x = 0; x < activeFacility.Size.x; x++ ) {
                 Vector3Int offset = new Vector3Int( x, y, 0 );
@@ -325,7 +263,6 @@ public class UIController : MonoBehaviour
         activeFacility.GetComponent<SpriteRenderer>().color = canPlace ? Color.green : Color.red;
     }
     public void ToggleRank( int index ) {
-
         SelectedEntity.Responsibilities[index] = !SelectedEntity.Responsibilities[index];
         NPCRoles[index].color = SelectedEntity.Responsibilities[index] ? Color.yellow : Color.black;
     }
