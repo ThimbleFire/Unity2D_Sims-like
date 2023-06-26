ca08be4c07c59033b3cd8b0c67f7225fef1fa9a5
Thimble <anthonyeboothroyd@gmail.com>
2023-06-26 03:59:40 +0100

    Update Knowldge.cs

--- SpaceSim-Master/Assets/Scripts/Knowldge.cs
+++ SpaceSim-Master/Assets/Scripts/Knowldge.cs
@@ -3,7 +3,44 @@ public class Knowledge
     public enum Category {
         Destination,            // navigations officer, captain
         ConditionFacility,      // engineer, gunnery
-        ConditionCrewMember,    // medic
-        Misc        
+        ConditionEntity,        // medic
     };
+
+    //These conditions should belong to the entity and facility classes
+    public enum ConditionFacility {
+        Broken,
+        NoPower,
+        Loaded,
+        Unloaded
+    };
+    public enum ConditionEntity {
+        Hungry,
+        Thirsty,
+        Bladdered,
+        WoundLight,
+        WoundMedium,
+        WoundBad,
+        WoundFatal,
+        Dead,
+        Happy,
+        Sad,
+        Depressed,
+        Suicidal
+    };
+    
+    private Category category;
+    private Facility facility;
+    private Entity entity;
+    private string destination;
+    
+    public Knowledge( ConditionFacility condition, Facility facility ) {
+        category = Category.ConditionFacility;
+    }
+    public Knowledge( ConditionEntity condition, Entity entity ) {
+        category = Category.ConditionEntity;
+    }
+    public Knowledge( string destination ) {
+        category = Category.Destination;
+    }    
+
 }
