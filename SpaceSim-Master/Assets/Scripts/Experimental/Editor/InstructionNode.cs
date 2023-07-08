f100aee9768a399e5db83411c89e4de2dba2e5d5
Thimble <anthonyeboothroyd@gmail.com>
2023-07-08 01:50:08 +0100

    Create InstructionNode.cs

---
+++ SpaceSim-Master/Assets/Scripts/Experimental/Editor/InstructionNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace AlwaysEast
+{
+    public class InstructionNode : Node
+    {
+        public string DialogueText;
+        public string GUID;
+        public bool EntyPoint = false;
+    }
+}
